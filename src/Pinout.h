735123de913a7c2f2547956238c2624347a405de
ott.dani96@hotmail.ch <Biberziefen96_z>
2021-04-23 15:53:58 +0200

    Removed test.cpp, Added variables to Lungs.h, Added Pinout.h

---
+++ src/Pinout.h
@@ -0,0 +1,14 @@
+//#### Chest-Compression ####
+
+//#### Eyes ####
+
+//#### Heart ####
+
+//#### Lungs ####
+
+//Input to read analog voltage from lung sensor in chest
+#define lung_inflation_input = ADC0;
+//Input to read analog voltage from lung sensor in chest
+#define ventilation_input = ADC1;
+
+//#### Speech ####
