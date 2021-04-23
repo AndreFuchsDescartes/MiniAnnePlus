527393d2f7c4ee78adb4a705a0a980ac739281bf
ott.dani96@hotmail.ch <Biberziefen96_z>
2021-04-23 16:41:40 +0200

    Modified Lungs.h

---
+++ src/Lungs.h
@@ -0,0 +1,28 @@
+// #### Settings ####
+
+//Upper limit of lung inflation. Values above this are concidered too little ventilation.
+double lung_inflation_threshold_upper = 0;
+//Lower limit of lung inflation. Values below this are concidered too much ventilation.
+double lung_inflation_threshold_lower = 0;
+//Upper limit of ventilation. Values above this are concidered as "NO ventilation is happening".
+double ventilation_threshold_upper = 0;
+//Lower limit of ventilation. Values below this are concidered as "ventilation IS happening".
+double ventilation_threshold_lower = 0;
+
+// #### Variables ####
+
+//Analog voltage from lung sensor in chest
+double lung_inflation; 
+//Indicates wheter lungs are inflated properley based on lung_inflation_threshold
+bool lung_inflation_is_good; 
+//Analog voltage from ventilation sensor in face
+double ventilation;
+//Timestamp to determine elapsed time between measurements
+bool ventilation_is_happening;
+//Timestamp to determine elapsed time between measurements
+int lung_millis_old;
+//Timestamp to determine elapsed time between measurements
+int lung_millis_new;
+
+// #### Functions ####
+
