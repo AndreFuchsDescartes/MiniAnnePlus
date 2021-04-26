fe8ccfccfa41819ce0fe9382379abd0a21dbdf84
AndreFuchsDescartes <andfuna@gmail.com>
2021-04-26 16:37:47 +0200

    updated eye_example.cpp

--- examples_for_.h-Files/Eyes_example.cpp
+++ examples_for_.h-Files/Eyes_example.cpp
@@ -2,18 +2,18 @@
 #include <Eyes.h>
 
 Adafruit_SSD1306 display1(128, 64, &Wire, -1);
-Eye e(display1);
+Eye eye_left(display1);
 
 //____________SETUP________________________________________________________
 void setup()
 {
- e.eyeSetup();
+ eye_left.eyeSetup();
 }
 
 //____________LOOP________________________________________________________
 void loop()
 {
-e.reactToLight();
+eye_left.reactToLight();
 }
 
 //____________DEVELOPER NOTES_____________________________________________
