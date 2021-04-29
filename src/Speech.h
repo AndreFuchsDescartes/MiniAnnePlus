f4a8496e7a5fdf03d9d366a5af038b52e0094ceb
AndreFuchsDescartes <andfuna@gmail.com>
2021-04-29 19:40:42 +0200

    startet speech.h

---
+++ src/Speech.h
@@ -0,0 +1,22 @@
+#ifdef Speech_h
+#define Speech_h
+class Speech
+{
+private:
+    int breathing_file_1
+    int rickroll = 1;
+    int coconut = 2;
+public:
+    Speech(/* args */);
+    ~Speech();
+};
+
+Speech::Speech(/* args */)
+{
+}
+
+Speech::~Speech()
+{
+}
+
+#endif
