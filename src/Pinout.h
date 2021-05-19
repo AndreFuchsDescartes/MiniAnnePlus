81cab848324f06ec81d80ec217e51dda2216dffc
ott.dani96@hotmail.ch <Biberziefen96_z>
2021-05-19 15:22:50 +0200

    adjustet pinout for mega shield konfiguration

--- src/Pinout.h
+++ src/Pinout.h
@@ -5,27 +5,26 @@
 //#### Chest-Compression ####
 
 //#### Eyes ####
-#define lightsensor_pin_left A2
-#define lightsensor_pin_right A3
+#define lightsensor_pin_left A12
+#define lightsensor_pin_right A13
 //input from RPie to controll Eyes
-#define EyeBit0 22
-#define EyeBit1 24
-#define EyeBit2 26
+#define EyeBit0 26
+#define EyeBit1 27
+#define EyeBit2 28
 //#### Heart ####
-#define position_recognition_hand_pin1 7
-#define position_recognition_hand_pin2 8
+#define position_recognition_hand_pin1 5
+#define position_recognition_hand_pin2 6
 //#### Lungs ####
 
 //Input pin to read analog voltage from lung sensor in chest
-#define lung_inflation_input_pin A1
+#define lung_inflation_input_pin A8
 //Input pin to read analog voltage from ventilation sensor in face
-#define ventilation_input_pin A0
-//Input pin to read analog voltage from cpr sensor
-#define cprInputPin A6
+#define cprInputPin A3
 
 //#### Speech ####
 #define mp3Player_rx 10
-#define mp3Player_tx 11  
-#define mp3Player_on 28
+#define mp3Player_tx 11
+
+#define mp3Player_on 29
 
 #endif
